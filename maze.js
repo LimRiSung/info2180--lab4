05d84ccffe4c7a46fc5720ea70ae9277d9f8d005
Chrissie <32311634+chrissiewilliams@users.noreply.github.com>
2018-10-22 07:24:07 -0500

    Half completed Exercise 2

--- maze.js
+++ maze.js
@@ -1,8 +1,8 @@
 /*Exercise 1 */
 
-var tlboundary;
+//var boundaries;
 
-window.onload = topLeftBoundary;
+window.onload = allBoundaries;
 
 function topLeftBoundary() {
    tlboundary = document.getElementById("boundary1");
@@ -14,7 +14,31 @@ function redBoundary() {
 }
 
 /*Exercise 2*/
+function allBoundaries() {
+    
+    var boundaries = document.querySelectorAll("boundary");
+    var i;
+    for (i = 0; i < boundaries.length; i++)
+    {
+        boundaries[i].onmouseover = allRedBoundary;
+        alert("What is going on?");
+    }
+}
 
+function allRedBoundary() {
+    var boundaries = document.querySelectorAll("boundary");
+    var i;
+    for (i = 0; i < boundaries.length; i++) {
+        boundaries[i].classList.add("youlose");
+       // alert("All should be red?");
+    }
+}
 
 /*Exercise 3*/
+
+
+
+/*Exercise 4*/
+
+/*Exercise 5*/
 
